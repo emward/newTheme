f8d9efa32469efe6874fa34080a2e55d53c00b3c
emward <eward@mcad.edu>
2013-02-14 23:29:00 -0600

    Updates to create a sidebar
    Changes to the index.php and style.css files to create a sidebar were
    made. Activated the sidebar in the functions.php document.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php  get_header (); ?>
-		<section>
+	<div class="clearfix">
+		<section class="main">
 			<img src="<?php bloginfo('stylesheet_directory'); ?>/images/hero.jpg" />
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<section>
@@ -14,4 +15,6 @@
             <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
             <?php endif; ?>
 		</section>
+        <?php get_sidebar(); ?>
+    </div>
 <?php get_footer(); ?>
